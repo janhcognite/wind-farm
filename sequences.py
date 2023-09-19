c02e55edb8dd2b8284f9293241e9f0553fb171dd
Jan Helge Sageflat <jan.helge.sageflat@cognite.com>
2023-09-19 22:47:57 +0200

    Update sequences.py

--- sequences.py
+++ sequences.py
@@ -1,14 +1,17 @@
-from cogniteapi import client
+from cogniteapitsp import client_tsp
 from cognite.client.data_classes.sequences import Sequence
 
 SEQ_EXTERNAL_ID = "V52-WindTurbine.MyTT"
 
-columns = [{"valueType": "LONG", "externalId": "timestamps", "description": "timestamps", "name": "timestamps"},
-           {"valueType": "STRING", "externalId": "values", "description": "values", "name": "values"}]
+columns = []
+
+for index in range(0, 300):
+    columns.append({"valueType": "DOUBLE", "externalId": str(index)})
+
 
 seq = Sequence(external_id=SEQ_EXTERNAL_ID, name=SEQ_EXTERNAL_ID, columns=columns)
 
-# client.sequences.create(seq)
+# client_tsp.sequences.create(seq)
 
 # seqs = client.sequences.list(name=SEQ_EXTERNAL_ID)
 
@@ -21,8 +24,8 @@ seq = Sequence(external_id=SEQ_EXTERNAL_ID, name=SEQ_EXTERNAL_ID, columns=column
 
 text = ""
 
-for i in range(1, 100):
-    text += "xxxxxxxxx_"
-    print("Length", len(text))
-    data = [(1, [0, text])]
-    client.sequences.data.insert(external_id=SEQ_EXTERNAL_ID, column_external_ids=["timestamps", "values"], rows=data)
+#seq = client_tsp.sequences.retrieve(external_id=SEQ_EXTERNAL_ID)
+
+rows = client_tsp.sequences.data.retrieve(external_id=SEQ_EXTERNAL_ID, start=240000, end=2500000, column_external_ids=["0"])
+
+print(rows)
