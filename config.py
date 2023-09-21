7669b870297bd09939949e45bc8ee795774de645
Jan Helge Sageflat <jan.helge.sageflat@cognite.com>
2023-09-21 23:18:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,13 @@ from datetime import datetime
 
 WT_DATA_SET_ID = 3387046938944729
 
+TS_NO_VALUE = 999999999
+
 DATA_SET_ID = 7914362746692288
 TOP_ASSET_EXT_ID = "wind_farm_01"
 TURBINE_COUNT = 9
 
-START_INSERT_DATE_JAN_2023 = int(datetime.strptime("2023-01-01", '%Y-%m-%d').timestamp()) * 1000
+JAN_2023_MS = int(datetime.strptime("2023-01-01", '%Y-%m-%d').timestamp()) * 1000
+JAN_2024_MS = int(datetime.strptime("2024-01-01", '%Y-%m-%d').timestamp()) * 1000
+
+TURBINE_WEEK_OFFSET = [0, 20, 16, 25, 5, 11, 2, 18, 7]
